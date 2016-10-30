a8e5ed5f53be09e37293974f70e5ecdc7b8faa60
hjpwhu <hjpwhu@qq.com>
2016-10-30 10:41:30 +0800

    read

--- src/hjp/mac/java/basis/demo/demo.java
+++ src/hjp/mac/java/basis/demo/demo.java
@@ -1,9 +1,40 @@
 package hjp.mac.java.basis.demo;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class demo {
-	
+
 	public static void main(String[] args) {
 		System.out.println("Hello, Java!");
+		String filePath = "/Users/hjp/Downloads/a.txt";
+		readFileByLines(filePath);
+	}
+
+	public static void readFileByLines(String fileName) {
+		File file = new File(fileName);
+		BufferedReader reader = null;
+		try {
+			reader = new BufferedReader(new FileReader(file));
+			String tempString = null;
+			int line = 1;
+			while ((tempString = reader.readLine()) != null) {
+				System.out.println("line " + line + ": " + tempString);
+				line++;
+			}
+			reader.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			if (reader != null) {
+				try {
+					reader.close();
+				} catch (IOException e1) {
+				}
+			}
+		}
 	}
 
 }
